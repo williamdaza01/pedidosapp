451d0604be7a75328df0735af95e02678a72fcc1
William Daza <williamdaza04@gmail.com>
2023-07-03 07:23:25 -0500

    vercel deploy

--- pedidosBack/pedidosapi/settings.py
+++ pedidosBack/pedidosapi/settings.py
@@ -129,3 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
 ]
+
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+
+WSGI_APPLICATION = 'pedidosapi.wsgi.application'
