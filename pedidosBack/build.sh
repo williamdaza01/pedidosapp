72e01b0ca03e1d3fc37325502c293e55a87ef798
William Daza <williamdaza04@gmail.com>
2023-07-04 07:24:09 -0500

    fix build

--- pedidosBack/build.sh
+++ pedidosBack/build.sh
@@ -7,6 +7,8 @@ python3.9 -m pip install -r requirements.txt
 echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
+python3.9 manage.py makemigrations pedidos --noinput
+python3.9 manage.py migrate pedidos --noinput
 
 echo "Collect Static..."
 python3.9 manage.py collectstatic --noinput --clear
